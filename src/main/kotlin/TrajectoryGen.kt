5bdb4bd33f4f91e4b284c7e1ffa84a391d39263a
kfajdsl <sahan.reddy.58@gmail.com>
2019-11-30 19:23:58 -0500

    Work

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -1,28 +1,35 @@
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator
-import com.acmerobotics.roadrunner.path.heading.HeadingInterpolator
-import com.acmerobotics.roadrunner.path.heading.SplineInterpolator
+import com.acmerobotics.roadrunner.path.heading.*
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
 
 object TrajectoryGen {
     private val constraints = DriveConstraints(45.0, 60.0, 0.0, 270.0.toRadians, 270.0.toRadians, 0.0)
-    private val startPose = Pose2d(-48.0, -24.0, 170.0.toRadians)
+    private val startPose = Pose2d(48.0, -28.0, 170.0.toRadians)
 
     fun createTrajectory(): ArrayList<Trajectory> {
         val list = ArrayList<Trajectory>()
 
-        val builder1 = TrajectoryBuilder(Pose2d(), constraints)
+        val builder1 = TrajectoryBuilder(startPose, constraints)
 
         // dump routine
-        /*builder
-            .setReversed(true)
-            .splineTo(Pose2d(-12.0, -42.0, 180.0.toRadians))// y = -39 is halfway between the skybridge and partner with 6" on either side, y = -42 gives 3" of room for other robot
-            .splineTo(Pose2d(28.0, -42.0, 180.0.toRadians))
-            .splineTo(Pose2d(48.0, -33.0, -90.0.toRadians))
-            .lineTo(Vector2d(48.0, -26.0), ConstantInterpolator(-90.0.toRadians))*/
+        builder1
+            //.lineTo( Vector2d(22.0, -63.0), ConstantInterpolator(90.0.toRadians))
+            //.splineTo(Pose2d(Vector2d(0.0, -36.0), 180.0.toRadians), LinearInterpolator(90.0.toRadians, 90.0.toRadians ))
+            .lineTo(Vector2d(36.0, -28.0), ConstantInterpolator(90.0.toRadians))
+
+        val builder2 = TrajectoryBuilder(Pose2d(36.0, -28.0, 0.0.toRadians), constraints)
+
+        builder2.lineTo(Vector2d(36.0, -63.0), ConstantInterpolator(0.0.toRadians))
+
+        val builder3 = TrajectoryBuilder(Pose2d(36.0, -63.0, 0.0.toRadians), constraints)
+
+        builder3
+            .back(16.0)
+            .strafeLeft(28.0)
+            .back(20.0)
 
 
         // weird reversed profiles
@@ -35,6 +42,8 @@ object TrajectoryGen {
 
 
         list.add(builder1.build())
+        list.add(builder2.build())
+        list.add(builder3.build())
         return list
     }
 
